6592fdd6419186d410b37b7235662bbadea85e35
eisn <eisnzwer@yandex.ru>
2024-05-13 20:05:07 +0300

    3 step: added Swagger and wrote xml comments

--- AuthPracticeProject/Controllers/HomeController.cs
+++ AuthPracticeProject/Controllers/HomeController.cs
@@ -1,8 +1,11 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
-namespace AuthPracticeProject.Controllers;
-
+namespace AuthPracticeProject.Controllers
+{
+	/// <summary>
+	/// Контроллер для управления домашней страницей.
+	/// </summary>
 	public class HomeController : Controller
 	{
 		private readonly ILogger<HomeController> _logger;
@@ -11,25 +14,37 @@ namespace AuthPracticeProject.Controllers;
 		{
 			_logger = logger;
 		}
-		
-		[HttpGet("")]
+        
+		/// <summary>
+		/// Основной контроллер для всех пользователей
+		/// </summary>
+		[HttpGet("Default")]
+		[ProducesResponseType(200, Type = typeof(string))]
 		public IActionResult Index()
 		{
 			return Ok("Основной контроллер");
 		}
 
+		/// <summary>
+		/// Защищенный метод для администраторов.
+		/// </summary>
 		[HttpGet("Secured/Admin")]
 		[Authorize(Policy = "AdminOnly")]
+		[ProducesResponseType(200, Type = typeof(string))]
 		public IActionResult SecuredMethodAdmin()
 		{
 			return Ok("Был получен доступ к защищенному методу для админов");
 		}
 
+		/// <summary>
+		/// Защищенный метод для пользователей старше 18 лет
+		/// </summary>
 		[HttpGet("Secured/Users")]
 		[Authorize(Policy = "UsersOnly")]
+		[ProducesResponseType(200, Type = typeof(string))]
 		public IActionResult SecuredMethodUsers()
 		{
 			return Ok("Был получен доступ к защищенному методу для пользователей старше 18 лет");
 		}
-		
 	}
+}
--- AuthPracticeProject/Program.cs
+++ AuthPracticeProject/Program.cs
@@ -1,39 +1,79 @@
+using System.Reflection;
 using System.Text;
 using AuthPracticeProject.Constants;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder();
 
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-	.AddJwtBearer(options =>
-	{
-		options.TokenValidationParameters = new TokenValidationParameters
-		{
-			ValidateIssuerSigningKey = true,
-			IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Consts.SECRET_KEY)),
-			ValidateIssuer = false,
-			ValidateAudience = false,
-			ValidateLifetime = true
-		};
-	});
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Consts.SECRET_KEY)),
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ValidateLifetime = true
+        };
+    });
 
 builder.Services.AddAuthorization(options =>
 {
-	options.AddPolicy("AdminOnly", policy =>
-		policy.RequireRole("Admin"));
-	options.AddPolicy("UsersOnly", policy =>
-		policy.RequireRole("User")
-			.RequireAssertion(context =>
-			{
-				var ageClaim = context.User.FindFirst("age");
-				if (ageClaim != null && int.TryParse(ageClaim.Value, out int age))
-				{
-					return age > 18;
-				}
-				return false;
-			}));
+    options.AddPolicy("AdminOnly", policy =>
+        policy.RequireRole("Admin"));
+    options.AddPolicy("UsersOnly", policy =>
+        policy.RequireRole("User")
+            .RequireAssertion(context =>
+            {
+                var ageClaim = context.User.FindFirst("age");
+                if (ageClaim != null && int.TryParse(ageClaim.Value, out int age))
+                {
+                    return age > 18;
+                }
+                return false;
+            }));
 });
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Authentication",
+        Description = "An ASP.NET Core Web API for authentication practice"
+    });
+
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+
+    // Добавление описания безопасности для Swagger
+    var jwtSecurityScheme = new OpenApiSecurityScheme
+    {
+        BearerFormat = "JWT",
+        Name = "JWT Authentication",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.Http,
+        Scheme = JwtBearerDefaults.AuthenticationScheme,
+        Description = "Put **_ONLY_** your JWT Bearer token on textbox below!",
+
+        Reference = new OpenApiReference
+        {
+            Id = JwtBearerDefaults.AuthenticationScheme,
+            Type = ReferenceType.SecurityScheme
+        }
+    };
+
+    options.AddSecurityDefinition(jwtSecurityScheme.Reference.Id, jwtSecurityScheme);
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        { jwtSecurityScheme, Array.Empty<string>() }
+    });
+});
+
 builder.Services.AddAuthorization();
 
 builder.Services.AddControllers();
@@ -45,5 +85,14 @@ app.MapControllers();
 app.UseAuthentication();
 app.UseAuthorization();
 
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        options.RoutePrefix = string.Empty;
+    });
+}
 
 app.Run();
