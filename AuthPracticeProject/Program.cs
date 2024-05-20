b7bdb4ec692b3d3a90af525097b076d5c93c6ea7
eisn <eisnzwer@yandex.ru>
2024-05-20 17:10:09 +0300

    fixed spaces instead of tabs in current files

--- AuthPracticeProject/Program.cs
+++ AuthPracticeProject/Program.cs
@@ -8,70 +8,69 @@ using Microsoft.OpenApi.Models;
 var builder = WebApplication.CreateBuilder();
 
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options =>
-    {
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuerSigningKey = true,
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Consts.SECRET_KEY)),
-            ValidateIssuer = false,
-            ValidateAudience = false,
-            ValidateLifetime = true
-        };
-    });
+	.AddJwtBearer(options =>
+	{
+		options.TokenValidationParameters = new TokenValidationParameters
+		{
+			ValidateIssuerSigningKey = true,
+			IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Consts.SECRET_KEY)),
+			ValidateIssuer = false,
+			ValidateAudience = false,
+			ValidateLifetime = true
+		};
+	});
 
 builder.Services.AddAuthorization(options =>
 {
-    options.AddPolicy("AdminOnly", policy =>
-        policy.RequireRole("Admin", "ADMIN"));
-    options.AddPolicy("UsersOnly", policy =>
-        policy.RequireRole("User", "USER")
-            .RequireAssertion(context =>
-            {
-                var ageClaim = context.User.FindFirst("age");
-                if (ageClaim != null && int.TryParse(ageClaim.Value, out int age))
-                {
-                    return age > 18;
-                }
-                return false;
-            }));
+	options.AddPolicy("AdminOnly", policy => policy.RequireRole("Admin", "ADMIN"));
+	options.AddPolicy("UsersOnly", policy => policy.RequireRole("User", "USER")
+		.RequireAssertion(context =>
+		{
+			var ageClaim = context.User.FindFirst("age");
+			if (ageClaim != null && int.TryParse(ageClaim.Value, out int age))
+			{
+				return age > 18;
+			}
+			return false;
+		}));
 });
 
 builder.Services.AddSwaggerGen(options =>
 {
-    options.SwaggerDoc("v1", new OpenApiInfo
-    {
-        Version = "v1",
-        Title = "Authentication",
-        Description = "An ASP.NET Core Web API for authentication practice"
-    });
+	options.SwaggerDoc("v1", new OpenApiInfo
+	{
+		Version = "v1",
+		Title = "Authentication",
+		Description = "An ASP.NET Core Web API for authentication practice"
+	});
 
-    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+	var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+	options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
 
-    // Добавление описания безопасности для Swagger
-    var jwtSecurityScheme = new OpenApiSecurityScheme
-    {
-        BearerFormat = "JWT",
-        Name = "JWT Authentication",
-        In = ParameterLocation.Header,
-        Type = SecuritySchemeType.Http, 
-        Scheme = JwtBearerDefaults.AuthenticationScheme,
-        Description = "Put **_ONLY_** your JWT Bearer token on textbox below!",
+	var jwtSecurityScheme = new OpenApiSecurityScheme
+	{
+		BearerFormat = "JWT",
+		Name = "JWT Authentication",
+		In = ParameterLocation.Header,
+		Type = SecuritySchemeType.Http,
+		Scheme = JwtBearerDefaults.AuthenticationScheme,
+		Description = "Put **_ONLY_** your JWT Bearer token on textbox below!",
 
-        Reference = new OpenApiReference
-        {
-            Id = JwtBearerDefaults.AuthenticationScheme,
-            Type = ReferenceType.SecurityScheme
-        }
-    };
+		Reference = new OpenApiReference
+		{
+			Id = JwtBearerDefaults.AuthenticationScheme,
+			Type = ReferenceType.SecurityScheme
+		}
+	};
 
-    options.AddSecurityDefinition(jwtSecurityScheme.Reference.Id, jwtSecurityScheme);
+	options.AddSecurityDefinition(jwtSecurityScheme.Reference.Id, jwtSecurityScheme);
 
-    options.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
-        { jwtSecurityScheme, Array.Empty<string>() }
-    });
+	options.AddSecurityRequirement(new OpenApiSecurityRequirement
+	{
+		{
+			jwtSecurityScheme, Array.Empty<string>()
+		}
+	});
 });
 
 builder.Services.AddAuthorization();
@@ -83,16 +82,17 @@ var app = builder.Build();
 app.MapControllers();
 
 app.UseAuthentication();
+
 app.UseAuthorization();
 
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI(options =>
-    {
-        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
-        options.RoutePrefix = string.Empty;
-    });
+	app.UseSwagger();
+	app.UseSwaggerUI(options =>
+	{
+		options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+		options.RoutePrefix = string.Empty;
+	});
 }
 
 app.Run();
